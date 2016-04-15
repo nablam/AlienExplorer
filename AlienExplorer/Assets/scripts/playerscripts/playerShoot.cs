65828e625fdb17037e51bc64f860794d11bef987
nablam <lamribenn@gmail.com>
2016-04-15 10:16:36 -0400

    misile launch

--- AlienExplorer/Assets/scripts/playerscripts/playerShoot.cs
+++ AlienExplorer/Assets/scripts/playerscripts/playerShoot.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
 namespace nabspace {
     public class playerShoot : MonoBehaviour
     {
 
-        // Use this for initialization
+        private GameManager_Master _gameManager;
+
+        public Transform cannontrans;
         void Start()
         {
+            _gameManager = GameObject.Find("GameManager_Object").GetComponent<GameManager_Master>();
+        }
+
+      
+        void Update()
+        {
+            if (_gameManager.isRocketMode) {
+                if (_gameManager.useAndroidControls) { AndroidShoot(); }
+                else
+                    pcShoot();
+            }
 
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        void AndroidShoot() {
+            if ( CrossPlatformInputManager.GetButtonUp("OnButtonShoot") ) {
+                Instantiate(Resources.Load("weapons/rocket_missile_prefab"), cannontrans.position, cannontrans.rotation); 
+            }
 
+        }
+        void pcShoot() {
+            if (Input.GetKeyUp("space")) { Instantiate(Resources.Load("weapons/rocket_missile_prefab"), cannontrans.position, cannontrans.rotation); }
         }
     }
 }
---
+++ AlienExplorer/Assets/scripts/weapons/rocket_missileScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class rocket_missileScript : MonoBehaviour {
+
+     float speed = 100f;
+    Transform playertrans;
+    Vector3 playerpos;
+
+	void Start () {
+
+        playertrans = GameObject.Find("rocketprefab").transform;
+
+    }
+
+
+    float distToRocket() {
+        Vector3 diff = transform.position - playerpos;
+
+        Debug.DrawLine(transform.position, playerpos, Color.red);
+
+        return diff.magnitude;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        playerpos = playertrans.position;
+        //  transform.Translate(transform.forward * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime*speed, Space.Self);       
+        checkdestroyByDistance();
+    }
+
+
+    void checkdestroyByDistance() {
+
+        if (distToRocket() > 100f) Destroy(gameObject);
+    }
+}
